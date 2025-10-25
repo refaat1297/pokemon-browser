71109e6e375e451b6de066e0ac3435c526ff0373
Ahmed Refaat <a.refaat1297@gmail.com>
2025-10-25 17:53:21 +0300

    feat: add PokemonCardSkeleton shared component

---
+++ src/components/shared/PokemonCardSkeleton.tsx
@@ -0,0 +1,13 @@
+const PokemonCardSkeleton = () => {
+  return (
+    <div className="bg-white rounded-lg shadow-md overflow-hidden border border-gray-100 animate-pulse">
+      <div className="aspect-square bg-gray-200"></div>
+      <div className="p-4 text-center">
+        <div className="h-6 bg-gray-200 rounded w-3/4 mx-auto"></div>
+        <div className="h-4 bg-gray-200 rounded w-1/2 mx-auto mt-2"></div>
+      </div>
+    </div>
+  );
+};
+
+export default PokemonCardSkeleton;
